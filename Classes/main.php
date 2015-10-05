7d768a95f359495ea98bfd44cfc241f66e22827a
Germangalia <germangalia@iesebre.com>
2015-10-05 21:02:09 +0200

    Final de classe.

--- Classes/main.php
+++ Classes/main.php
@@ -5,9 +5,10 @@
  * Date: 5/10/15
  * Time: 20:25
  */
+
+require_once __DIR__ . '/vendor/autoload.php';
 use Com\Iesebre\Dam2\germangalia\Persons\Person;
 
-require "/home/ggalia84/Code/School/Classes/Persons/Person.php";
 
 $person = new Person();
 
